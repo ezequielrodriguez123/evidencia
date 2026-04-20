e05f730c9b16f997b5243bd5a269f51d2ed0e11b
ezequielrodriguez123 <ezequielgameon@gmail.com>
2026-04-20 13:31:31 -0600

    codigo arreglado

--- evidencia3.py
+++ evidencia3.py
@@ -1,29 +1,40 @@
 class Persona:
-    pass
-class Persona:
     def __init__(self, nombre, edad, altura, peso, correo):
         self.__nombre = nombre
         self.__edad = edad
         self.__altura = altura
         self.__peso = peso
         self.__correo = correo
+
+    # Getters
     def get_nombre(self):
         return self.__nombre
 
     def get_edad(self):
         return self.__edad
-        def set_nombre(self, nombre):
-            self.__nombre = nombre
+
+    # Setters
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
 
     def set_edad(self, edad):
         self.__edad = edad
+
+    # Método 1
     def calcular_imc(self):
         return self.__peso / (self.__altura ** 2)
+
+    # Método 2
     def es_mayor_de_edad(self):
         return self.__edad >= 18
-        def info(self):
-            return f"Nombre: {self.__nombre}, Edad: {self.__edad}, Correo: {self.__correo}"
-        persona1 = Persona("Ezequiel", 18, 1.75, 70, "correo@gmail.com")
+
+    # Método info
+    def info(self):
+        return f"Nombre: {self.__nombre}, Edad: {self.__edad}, Correo: {self.__correo}"
+
+
+# Prueba
+persona1 = Persona("Ezequiel", 18, 1.75, 70, "correo@gmail.com")
 
 print(persona1.info())
 print("IMC:", persona1.calcular_imc())
