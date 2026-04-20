ae67071b788365bdbdc73be4b2fd15dfde773d05
ezequielrodriguez123 <ezequielgameon@gmail.com>
2026-04-20 13:25:30 -0600

    Se agregan atributos privados

--- evidencia3.py
+++ evidencia3.py
@@ -1,2 +1,9 @@
 class Persona:
     pass
+class Persona:
+    def __init__(self, nombre, edad, altura, peso, correo):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__altura = altura
+        self.__peso = peso
+        self.__correo = correo
